92537baf79909393929fbcb2e76568b2baa310ef
LOKESHKARMOKAR <146501836+LOKESHKARMOKAR@users.noreply.github.com>
2024-08-31 23:15:21 +0530

    37th

---
+++ script.js
@@ -0,0 +1,83 @@
+let score = 50;
+// let result = score * 2 + (4*3) -8/2%4;
+// console.log(result);
+
+// let result2 = "My total 'score' is: " + score;
+// console.log(result2);
+
+// let age = "22";
+// console.log(typeof(age));
+// let n = Number(age);
+// console.log(n, typeof(n));
+
+/// Array
+
+// let dishes = ["Biriyani", "Fride rice", "Ice cream", "Gulab jamun"];
+// console.log(dishes);
+// console.log(dishes[3]);
+// dishes[1] = "chili chicken";
+// console.log(dishes);
+// console.log(dishes.join(' - '));
+// console.log(dishes.indexOf("Ice cream"));
+// let newDishes = ["Mutton kosa", "Rosogolla", "Fish Fry"];
+// console.log(dishes.concat(newDishes));
+// console.log(dishes.length);
+// console.log(dishes.push("Rabri"), dishes);
+// let r = dishes.pop();
+// console.log(r);
+// console.log(dishes);
+
+// let arr = ["apple", 20, 20.5, true, 'a'];
+// console.log(arr);
+
+// for loop
+
+// for(let i =1; i<=10; i++){
+//     console.log("Run task", i);
+    
+// }
+
+ //let dishes = ["Biriyani", "Fride rice", "Ice cream", "Gulab jamun"];
+// for(let i=0; i<dishes.length; i++){
+//     console.log(dishes[i]);
+    
+// }
+
+// let i =1
+// while(i<=10){
+//     console.log(i);
+//     i++
+    
+// }
+
+// let age = 17;
+// if(age >= 18){
+//     console.log("Are you Major");
+    
+// }else{
+//     console.log("are you minor");
+    
+// }
+// let age = 12;
+
+// let result = age <18 ? "not eligible for vote" : "eligible for vote";
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
